d09c11b276005fcf9c9d69d18fdf7cc2fa6f29ae
greguz <gregolettogiacomo@gmail.com>
2023-10-24 09:06:54 +0200

    Add types

---
+++ fnap.d.ts
@@ -0,0 +1,33 @@
+import { FastifyPluginCallback } from 'fastify'
+
+export interface FastifyNoAdditionalPropertiesOptions {
+  /**
+   * If true, update the request body schema.
+   * @default true
+   */
+  body?: boolean
+  /**
+   * If true, update the request headers schema.
+   * @default false
+   */
+  headers?: boolean
+  /**
+   * If true, update the URL parameters schema.
+   * @default false
+   */
+  params?: boolean
+  /**
+   * If true, update the URL querystring schema.
+   * @default true
+   */
+  query?: boolean
+  /**
+   * If true, update all response schemas.
+   * @default false
+   */
+  response?: boolean
+}
+
+declare const plugin: FastifyPluginCallback<FastifyNoAdditionalPropertiesOptions>
+
+export default plugin
